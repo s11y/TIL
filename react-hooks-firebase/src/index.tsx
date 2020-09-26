e0a683601257651c324c48a1455a2ed0c70ee271
ShinokiRyosei <shinoki.r.0301@gmail.com>
2020-09-26 20:35:28 +0900

    react hooks and firebase

---
+++ react-hooks-firebase/src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Route exact path="/" component={App} />
+      <Route exact path="/login" component={Login} />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
